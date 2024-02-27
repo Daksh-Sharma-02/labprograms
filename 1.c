acd4accf8012fbf576630002d0b1af018b52b71e
Daksh-Sharma-02 <159519639+Daksh-Sharma-02@users.noreply.github.com>
2024-02-27 11:48:36 +0530

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,33 @@
+// Linear search
+#include<stdio.h>
+#include<stdbool.h>
+int main()
+{
+    int n,index;
+    printf("Enter the Size of an Array : ");
+    scanf("%d",&n);
+    
+    printf("Enter the elements of Array \n");
+    
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+
+    int x;
+    printf("Enter the element to be searched \n");
+    scanf("%d",&x);
+
+    bool flag = false;
+    for(int i=0;i<n;i++){
+        if(arr[i]==x){
+            flag = true;
+            index=i;
+            break;
+        }
+    }
+
+    if(flag==true) printf("%d Exists in an array at %d ",x,index);
+    if(flag==false) printf("%d  doesnot Exists in an array at %d ",x,index);
+
+}
