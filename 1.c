55c0f53bb85df4e963300bd2b152aaf2f09df745
Daksh-Sharma-02 <159519639+Daksh-Sharma-02@users.noreply.github.com>
2024-02-28 21:35:38 +0530

    Update 1.c

--- 1.c
+++ 1.c
@@ -1,35 +1,37 @@
-// Linear search
+// Implement a List using Array and develop functions to perform insertion operation
 #include<stdio.h>
-#include<stdbool.h>
-int main()
-{
-    int n,index;
-    printf("Enter the Size of an Array : ");
-    scanf("%d",&n);
-    
-    printf("Enter the elements of Array \n");
-    
-    int arr[n];
-    for(int i=0;i<n;i++){
-        scanf("%d",&arr[i]);
+void display(int arr[], int n) {
+    for(int i=0; i<n; i++) {
+        printf("%d ", arr[i]);
     }
-
-    int x;
-    printf("Enter the element to be searched \n");
-    scanf("%d",&x);
-
-    bool flag = false;
-    for(int i=0;i<n;i++){
-        if(arr[i]==x){
-            flag = true;
-            index=i;
-            break;
-        }
+    printf("\n");
+}
+void insertion(int arr[], int n, int element, int capacity, int index) {
+    if(n>=capacity) {
+        printf("Array is full. Cannot insert.\n");
     }
-
-    if(flag==true) printf("%d Exists in an array at %d ",x,index);
-    if(flag==false) printf("%d  doesnot Exists in an array at %d ",x,index);
-
+    for(int i=n-1; i>=index; i--) {
+        arr[i+1]=arr[i];
+    }
+    arr[index]=element;
+}
+int main() {
+    int arr[1000], n, element, index, capacity=1000;
+    printf("Enter the size of array :\n");
+    scanf("%d", &n);
+    printf("Enter %d elements :\n", n);
+    for(int i=0; i<n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    printf("The array is : \n");
+    display(arr, n);
+    printf("Enter the index where you want to insert an element : ");
+    scanf("%d", &index);
+    printf("Enter the element to insert :\n");
+    scanf("%d", &element);
+    printf("The resultant array is :\n");
+    insertion(arr, n, element, capacity, index);
+    n+=1;
+    display(arr, n);
     return 0;
-   
 }
